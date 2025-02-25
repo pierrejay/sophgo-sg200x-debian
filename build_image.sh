9931b8ec85faac5ead28a9a7376c95cde9578088
Pierre Jay <p.jay@orius.co>
2025-02-25 02:04:33 +0100

    Edit build_image.sh w/ duos

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run --privileged -it --rm -v ./configs/:/configs -v ./image:/output builder make BOARD=licheervnano image
+#docker run --privileged -it --rm -v ./configs/:/configs -v ./image:/output builder make BOARD=licheervnano image
+docker run --privileged -it --rm -v ./configs/:/configs -v ./image:/output builder make BOARD=duos image
